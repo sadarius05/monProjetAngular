3f3b603421d9192546d96d37eeb3b0465e978941
Souliman <souleymane.sada@gmail.com>
2023-01-11 02:09:12 +0000

    Gestion des donnees avec le data-binding: proprity-binding, l'event-binding, le two-way binding

--- mon-projet-angular/src/app/app.component.ts
+++ mon-projet-angular/src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  [x: string]: string;
+  isAuth = false;
+
+  appareilOne = 'Machine a laver';
+  appareilTwo = 'Television';
+  appareilThree = 'Ordinateur';
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
+  }
+
+  onAllumer() {
+    console.log('Tout est allume !');
+  }
 }
